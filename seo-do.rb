6af7b50f3a4a0a1ebfe6afb5223a3cbb80ac53d1
Metehan Çetinkaya <metehancet@gmail.com>
2020-04-29 16:37:09 +0300

    Changed class name

--- seo-do.rb
+++ seo-do.rb
@@ -1,4 +1,4 @@
-class SeoDo < Formula
+class Seodo < Formula
   include Language::Python::Virtualenv
 
   desc "An SEO cli that help SEO professionals to organize keywords by different grouping algorithms"
