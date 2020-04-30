6d8ed69d4fa8bec6510a4ee15be1fa23797e6324
Metehan Çetinkaya <metehancet@gmail.com>
2020-04-30 14:43:12 +0300

    Small Changes

--- seodo.rb
+++ seodo.rb
@@ -76,9 +76,14 @@ class Seodo < Formula
     sha256 "99cf342406f3499cec43270fcfaf93c126c5164092eca201dfef0f623360b409"
   end
 
+  resource "pathlib2" do
+    url: "https://files.pythonhosted.org/packages/94/d8/65c86584e7e97ef824a1845c72bbe95d79f5b306364fa778a3c3e401b309/pathlib2-2.3.5.tar.gz"
+    "6cd9a47b597b37cc57de1c05e56fb1a1c9cc9fab04fe78c29acd090418529868"
+  end
+
   def install
     venv = virtualenv_create(libexec)
-    %w[poetry clikit cleo].each do |r|
+    %w[pathlib2 poetry clikit cleo].each do |r|
         venv.pip_install resource(r)
     end
     venv.pip_install resources
