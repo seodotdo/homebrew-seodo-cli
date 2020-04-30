95d4c81db68f4d5ab6d9c6c7a6ca2283797c137f
Metehan Çetinkaya <metehancet@gmail.com>
2020-04-30 13:30:09 +0300

    Added poetry dependency

--- seodo.rb
+++ seodo.rb
@@ -10,6 +10,7 @@ class Seodo < Formula
   revision 1
 
   depends_on "python@3"
+  depends_on "poetry"
 
   resource "setuptools" do
     url "https://files.pythonhosted.org/packages/df/ed/bea598a87a8f7e21ac5bbf464102077c7102557c07db9ff4e207bd9f7806/setuptools-46.0.0.zip"
