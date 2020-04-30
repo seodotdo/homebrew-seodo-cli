e7b132e1610e008f61b10bea2c3397545bfa7c83
Metehan Çetinkaya <metehancet@gmail.com>
2020-04-30 14:44:15 +0300

    Small Changes

--- seodo.rb
+++ seodo.rb
@@ -77,8 +77,8 @@ class Seodo < Formula
   end
 
   resource "pathlib2" do
-    url: "https://files.pythonhosted.org/packages/94/d8/65c86584e7e97ef824a1845c72bbe95d79f5b306364fa778a3c3e401b309/pathlib2-2.3.5.tar.gz"
-    "6cd9a47b597b37cc57de1c05e56fb1a1c9cc9fab04fe78c29acd090418529868"
+    url "https://files.pythonhosted.org/packages/94/d8/65c86584e7e97ef824a1845c72bbe95d79f5b306364fa778a3c3e401b309/pathlib2-2.3.5.tar.gz"
+    sha256 "6cd9a47b597b37cc57de1c05e56fb1a1c9cc9fab04fe78c29acd090418529868"
   end
 
   def install
